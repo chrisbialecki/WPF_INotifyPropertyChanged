30c25342f5d47035719947c9d699fbdeef393a1a
Krzysztof Bialecki <chrisbialecki@outlook.ie>
2016-06-30 19:15:23 +0100

    go

---
+++ WPF_INotifyPropertyChanged_3/BoundValueModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_INotifyPropertyChanged_3
+{
+    public class BoundValueModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        string _boundValue = "start-up bound value";
+
+        public string BoundValue
+        {
+            get { return _boundValue; }
+            set
+            {
+                _boundValue = value;
+                OnPropertyChanged("BoundValue");
+            }
+        }
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
